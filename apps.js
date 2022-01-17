d1de2e18c861d3010db36d5b1170cf89592a798f
William Busby <williambusby1999@gmail.com>
2022-01-17 16:52:40 +0000

    Moved script to apps.js and started techstack sect

---
+++ apps.js
@@ -0,0 +1,10 @@
+function navHideShow() {
+
+  document.getElementById('drop-down-anchor').style.display = 'none';
+  document.getElementById('drop-down').style.display = 'flex';
+}
+
+function navShowHide() {
+  document.getElementById('drop-down-anchor').style.display = 'flex';
+  document.getElementById('drop-down').style.display = 'none';
+}
