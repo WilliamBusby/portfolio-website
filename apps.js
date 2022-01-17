2129bf272ce8dd232539e6856019661b6b0ccd14
William Busby <williambusby1999@gmail.com>
2022-01-17 17:51:13 +0000

    Centered techstack text

--- apps.js
+++ apps.js
@@ -17,14 +17,14 @@ function circularText(txt, radius, classIndex) {
   txt = txt.split(""),
     classIndex = document.getElementsByClassName("circText")[classIndex];
 
-  let deg = 100 / txt.length,
-    origin = -50;
+  let deg = 120 / txt.length,
+    origin = -60;
 
   txt.forEach((ea) => {
-    ea = `<p style='height:${radius}vw;position:absolute;transform:rotate(${origin}deg);transform-origin: 0 100%;right:5vw;bottom:5vw'>${ea}</p>`;
+    ea = `<p style='height:${radius}vw;position:absolute;transform:rotate(${origin}deg);transform-origin: 0 100%;right: 4.5vw;bottom: 5vw;'>${ea}</p>`;
     classIndex.innerHTML += ea;
     origin += deg;
   });
 }
 
-circularText("TECHSTACK", 7, 0)
+circularText("TECHSTACK", 8, 0)
--- dist/apps.dev.js
+++ dist/apps.dev.js
@@ -14,13 +14,13 @@ function navShowHide() {
 
 function circularText(txt, radius, classIndex) {
   txt = txt.split(""), classIndex = document.getElementsByClassName("circText")[classIndex];
-  var deg = 100 / txt.length,
-      origin = -50;
+  var deg = 120 / txt.length,
+      origin = -60;
   txt.forEach(function (ea) {
-    ea = "<p style='height:".concat(radius, "vw;position:absolute;transform:rotate(").concat(origin, "deg);transform-origin: 0 100%;right:5vw;bottom:5vw'>").concat(ea, "</p>");
+    ea = "<p style='height:".concat(radius, "vw;position:absolute;transform:rotate(").concat(origin, "deg);transform-origin: 0 100%;right: 4.5vw;bottom: 5vw;'>").concat(ea, "</p>");
     classIndex.innerHTML += ea;
     origin += deg;
   });
 }
 
-circularText("TECHSTACK", 7, 0);
+circularText("TECHSTACK", 8, 0);
