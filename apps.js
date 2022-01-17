460c07dd1a27581199ec610bfd1947c99b8a30c6
William Busby <williambusby1999@gmail.com>
2022-01-17 17:23:05 +0000

    Circular text added for techstack

--- apps.js
+++ apps.js
@@ -1,3 +1,5 @@
+// Navigation bar
+
 function navHideShow() {
 
   document.getElementById('drop-down-anchor').style.display = 'none';
@@ -8,3 +10,21 @@ function navShowHide() {
   document.getElementById('drop-down-anchor').style.display = 'flex';
   document.getElementById('drop-down').style.display = 'none';
 }
+
+// Techstack
+
+function circularText(txt, radius, classIndex) {
+  txt = txt.split(""),
+    classIndex = document.getElementsByClassName("circText")[classIndex];
+
+  let deg = 100 / txt.length,
+    origin = -50;
+
+  txt.forEach((ea) => {
+    ea = `<p style='height:${radius}vw;position:absolute;transform:rotate(${origin}deg);transform-origin: 0 100%;right:5vw;bottom:5vw'>${ea}</p>`;
+    classIndex.innerHTML += ea;
+    origin += deg;
+  });
+}
+
+circularText("TECHSTACK", 7, 0)
